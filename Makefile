16363dc2e141f1758caca3d1b8f580e03033ef57
Gustav Behm <gustav.behm@gmail.com>
2016-05-12 19:23:32 +0200

    Start of a microGranny program to keep sent notes playing until a new one comes along

--- Makefile
+++ Makefile
@@ -22,7 +22,14 @@ CHORDIFY_OUTPUT=$(shell $(GET_CONFIG) chordify output)
 CHORDIFY_SERVICE=chordify.service
 CHORDIFY_SERVICE_FILE=$(SERVICES)/$(CHORDIFY_SERVICE)
 
-install: unlegato chordify
+MICROGRANNY_BIN=$(BUILD)/microGranny
+MICROGRANNY_CHANNEL=$(shell $(GET_CONFIG) microGranny channel)
+MICROGRANNY_INPUT=$(shell $(GET_CONFIG) microGranny input)
+MICROGRANNY_OUTPUT=$(shell $(GET_CONFIG) microGranny output)
+MICROGRANNY_SERVICE=microGranny.service
+MICROGRANNY_SERVICE_FILE=$(SERVICES)/$(MICROGRANNY_SERVICE)
+
+install: unlegato microGranny
 
 dependencies:
 	sudo apt install cmake libasound2-dev crudini
@@ -51,7 +58,19 @@ $(CHORDIFY_SERVICE_FILE): $(CHORDIFY_BIN) $(CONFIG)
 	sudo sed -i "s#%%OUTPUT%%#$(CHORDIFY_OUTPUT)#" $@
 	sudo systemctl enable $(CHORDIFY_SERVICE)
 
-.PHONY: $(UNLEGATO_BIN) $(CHORDIFY_BIN)
+.PHONY: microGranny
+microGranny: $(MICROGRANNY_BIN) $(MICROGRANNY_SERVICE_FILE)
+	sudo systemctl start $(MICROGRANNY_SERVICE)
+
+$(MICROGRANNY_SERVICE_FILE): $(MICROGRANNY_BIN) $(CONFIG)
+	sudo install -D $(SERVICES_TEMPLATE) $@
+	sudo sed -i "s#%%EXECUTABLE%%#$(shell readlink -f $<) $(MICROGRANNY_CHANNEL)#" $@
+	sudo sed -i "s#%%SERVICE%%#microGranny#" $@
+	sudo sed -i "s#%%INPUT%%#$(MICROGRANNY_INPUT)#" $@
+	sudo sed -i "s#%%OUTPUT%%#$(MICROGRANNY_OUTPUT)#" $@
+	sudo systemctl enable $(MICROGRANNY_SERVICE)
+
+.PHONY: $(UNLEGATO_BIN) $(CHORDIFY_BIN) $(MICROGRANNY_BIN)
 $(CHORDIFY_BIN) $(UNLEGATO_BIN): | $(BUILD) dependencies
 	cd $(BUILD); cmake -DCMAKE_BUILD_TYPE=Release ..
 	cd $(BUILD); make
---
+++ microGranny.cpp
@@ -0,0 +1,60 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <set>
+#include <midi.hpp>
+#include <iostream>
+
+inline bool allowedMicroGrannyNote(const midi::Command::Type::NoteOn note) {
+    return (note->key >= 1 && note->key <= 6);
+}
+
+int main(int argc, const char* argv[])
+{
+    if(argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " CHANNEL" << std::endl;
+        return 2;
+    }
+
+    midi::Channel mgChannel = std::stoi(argv[1]);
+    std::cout << "microGranny-ing MIDI-channel " << unlegatoChannel << std::endl;
+
+    midi::Interface interface("microGranny");
+
+    std::set<midi::Key> activeNotes;
+
+    while (1)
+    {
+        midi::Command::ptr command = interface.nextCommand();
+        if(command->getType() == midi::Command::Type::NoteOn)
+        {
+            auto noteOnCommand = std::dynamic_pointer_cast<midi::NoteOn>(command);
+            if(noteOnCommand->getChannel() == mgChannel && allowedMicroGrannyNote(noteOnCommand))
+            {
+                for (auto key : activeNotes)
+                    interface.sendCommand(midi::Command::ptr(new midi::NoteOff(noteOnCommand->getChannel(), key)));
+                activeNotes.clear();
+                activeNotes.insert(noteOnCommand->getKey());
+                interface.sendCommand(command);
+            }
+        }
+        else if(command->getType() == midi::Command::Type::TimingClock)
+        {
+            interface.sendCommand(command);
+        }
+
+    }
+
+    return 0;
+}
