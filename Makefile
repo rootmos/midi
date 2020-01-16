07fdababcbbdfaa42a5fcb2200cfcfc6ef11d1c3
Gustav Behm <me@rootmos.io>
2020-01-16 13:05:41 +0100

    Add deployment tools

--- Makefile
+++ Makefile
@@ -5,6 +5,13 @@ all:
 	@mkdir -p "$(BUILD)"
 	$(MAKE) -C src
 
+midi.tar.gz: all
+	scripts/bundle.sh -f "$@" -b "$(BUILD)" -r "$(ROOT)/root"
+
+deploy: midi.tar.gz
+	scp $< midi:
+	ssh midi sudo tar xf $< -C /
+
 clean:
 	rm -rf $(BUILD)
 
---
+++ scripts/bundle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o nounset -o pipefail -o errexit
+
+TMP=$(mktemp -d)
+trap 'rm -rf $TMP' EXIT
+
+while getopts "f:r:b:-" OPT; do
+    case $OPT in
+        f) OUT=$OPTARG ;;
+        r) ROOT=$OPTARG ;;
+        b) BUILD=$OPTARG ;;
+        -) break ;;
+        ?) exit 2 ;;
+    esac
+done
+shift $((OPTIND-1))
+
+PREFIX=${PREFIX-/usr/local/bin}
+
+install -D -t "$TMP/$PREFIX" "$BUILD"/*
+tar -cf- -C "$ROOT" . | tar -xf- -C "$TMP"
+
+tar -cf "$OUT" -C "$TMP" .
