295239bcc618be3d3fd64026e7517374422f5288
Gustav Behm <me@rootmos.io>
2020-06-30 21:53:25 +0200

    Store and enqueue scenes of offsets in the spazer

--- src/spazer.c
+++ src/spazer.c
@@ -1,15 +1,23 @@
 #include "midi.c"
 
-struct note {
-    unsigned int offset;
-    unsigned char controller_number;
+struct scene {
+    size_t i;
+    unsigned int offset[256];
+
+    unsigned char key;
+    struct timespec key_down;
 };
 
 struct state {
     unsigned char note_channel;
     unsigned char control_channel;
-    struct note note[256];
-    size_t notes;
+
+    struct scene live;
+    struct scene* queue;
+    struct scene stored[12];
+
+    unsigned char controller_number[256];
+    size_t offsets;
 
     unsigned char active_notes[32];
     unsigned char active_notes_velocity[32];
@@ -22,6 +30,20 @@ static void reset_active_notes(struct state* st)
     }
 }
 
+static void consider_scene_switch(struct state* st)
+{
+    if(st->queue == NULL) return;
+
+    for(size_t i = 0; i < LENGTH(st->active_notes); i++) {
+        if(!(st->active_notes[i] >> 7)) return;
+    }
+
+    info("switching to scene: %zu", st->queue->i + 1);
+    memcpy(&st->live, st->queue, sizeof(st->live));
+
+    st->queue = NULL;
+}
+
 static void handle_note_on(struct ctx* ctx, struct state* st,
                            snd_seq_event_t* ev)
 {
@@ -29,9 +51,9 @@ static void handle_note_on(struct ctx* ctx, struct state* st,
     debug("note on: %u", base);
 
     send_event(ctx, ev);
-    for(size_t i = 0; i < st->notes; i++) {
-        if(st->note[i].offset == 0) continue;
-        ev->data.note.note = base + st->note[i].offset;
+    for(size_t i = 0; i < st->offsets; i++) {
+        if(st->live.offset[i] == 0) continue;
+        ev->data.note.note = base + st->live.offset[i];
         send_event(ctx, ev);
     }
 
@@ -51,9 +73,9 @@ static void handle_note_off(struct ctx* ctx, struct state* st,
     debug("note off: %u", base);
 
     send_event(ctx, ev);
-    for(size_t i = 0; i < st->notes; i++) {
-        if(st->note[i].offset == 0) continue;
-        ev->data.note.note = base + st->note[i].offset;
+    for(size_t i = 0; i < st->offsets; i++) {
+        if(st->live.offset[i] == 0) continue;
+        ev->data.note.note = base + st->live.offset[i];
         send_event(ctx, ev);
     }
 
@@ -63,6 +85,8 @@ static void handle_note_off(struct ctx* ctx, struct state* st,
             break;
         }
     }
+
+    consider_scene_switch(st);
 }
 
 static void handle_keypress(struct ctx* ctx, struct state* st,
@@ -72,9 +96,9 @@ static void handle_keypress(struct ctx* ctx, struct state* st,
     debug("keypress: %u", base);
 
     send_event(ctx, ev);
-    for(size_t i = 0; i < st->notes; i++) {
-        if(st->note[i].offset == 0) continue;
-        ev->data.note.note = base + st->note[i].offset;
+    for(size_t i = 0; i < st->offsets; i++) {
+        if(st->live.offset[i] == 0) continue;
+        ev->data.note.note = base + st->live.offset[i];
         send_event(ctx, ev);
     }
 }
@@ -82,11 +106,11 @@ static void handle_keypress(struct ctx* ctx, struct state* st,
 static void change_offset(struct ctx* ctx, struct state* st,
                           unsigned char param, unsigned char value)
 {
-    for(size_t i = 0; i < st->notes; i++) {
-        if(param != st->note[i].controller_number) continue;
+    for(size_t i = 0; i < st->offsets; i++) {
+        if(param != st->controller_number[i]) continue;
 
-        unsigned char prev = st->note[i].offset;
-        st->note[i].offset = value;
+        unsigned char prev = st->live.offset[i];
+        st->live.offset[i] = value;
         info("setting note %zu: offset=%u", i + 1, value);
 
         for(size_t j = 0; j < LENGTH(st->active_notes); j++) {
@@ -109,19 +133,50 @@ static void change_offset(struct ctx* ctx, struct state* st,
     }
 }
 
-int go(struct ctx* ctx, snd_seq_event_t* ev, void* opaque)
+static void handle_scene_key_down(struct ctx* ctx, struct state* st,
+                                  unsigned char key)
+{
+    struct timespec t;
+    int r = clock_gettime(CLOCK_MONOTONIC_RAW, &t);
+    CHECK(r, "clock_gettime(CLOCK_MONOTONIC_RAW)");
+
+    struct scene* s = &st->stored[key % 12];
+
+    unsigned int ms =
+        (t.tv_sec - s->key_down.tv_sec)*1000 +
+        (t.tv_nsec - s->key_down.tv_nsec) / 1000000;
+
+    debug("scene key press: i=%zu ms=%u note=%u", s->i, ms, key);
+
+    if(ms > 3000) {
+        info("storing scene %zu", s->i + 1);
+        memcpy(s, &st->live, sizeof(*s));
+    } else {
+        st->queue = s;
+        consider_scene_switch(st);
+    }
+}
+
+static int go(struct ctx* ctx, snd_seq_event_t* ev, void* opaque)
 {
     struct state* st = opaque;
 
     switch(ev->type) {
     case SND_SEQ_EVENT_NOTEON:
+        // TODO: use the CHANNEL macro
         if(st->note_channel == ev->data.note.channel) {
             handle_note_on(ctx, st, ev);
+        } else if(st->control_channel == ev->data.note.channel) {
+            int r = clock_gettime(CLOCK_MONOTONIC_RAW,
+                          &st->stored[ev->data.note.note % 12].key_down);
+            CHECK(r, "clock_gettime(CLOCK_MONOTONIC_RAW)");
         }
         break;
     case SND_SEQ_EVENT_NOTEOFF:
         if(st->note_channel == ev->data.note.channel) {
             handle_note_off(ctx, st, ev);
+        } else if(st->control_channel == ev->data.note.channel) {
+            handle_scene_key_down(ctx, st, ev->data.note.note);
         }
         break;
     case SND_SEQ_EVENT_KEYPRESS:
@@ -130,15 +185,15 @@ int go(struct ctx* ctx, snd_seq_event_t* ev, void* opaque)
         }
         break;
     case SND_SEQ_EVENT_CONTROLLER:
-        debug("cc %u %u %u", ev->data.control.channel, ev->data.control.param, ev->data.control.value);
+        debug("cc %u %u %u", ev->data.control.channel, ev->data.control.param,
+              ev->data.control.value);
         if(st->control_channel == ev->data.control.channel) {
             change_offset(ctx, st,
                           ev->data.control.param,
                           ev->data.control.value);
         } else if(st->note_channel == ev->data.control.channel) {
             send_event(ctx, ev);
-            if(ev->data.control.param == 123 &&
-               ev->data.control.value == 0) {
+            if(ev->data.control.param == 123 && ev->data.control.value == 0) {
                 reset_active_notes(st);
             }
         }
@@ -166,25 +221,26 @@ int main(int argc, char* argv[])
         return 2;
     }
 
-    struct state st = {
-        .note_channel = -1,
-        .control_channel = -1,
-        .notes = 0,
-    };
+    struct state st;
+    memset(&st, 0, sizeof(st));
+    st.note_channel = -1;
+    st.control_channel = -1;
 
-    st.note[st.notes++] = (struct note) { .controller_number = 110 };
-    st.note[st.notes++] = (struct note) { .controller_number = 111 };
-    st.note[st.notes++] = (struct note) { .controller_number = 112 };
-    st.note[st.notes++] = (struct note) { .controller_number = 113 };
-    st.note[st.notes++] = (struct note) { .controller_number = 17 };
-    st.note[st.notes++] = (struct note) { .controller_number = 91 };
-    st.note[st.notes++] = (struct note) { .controller_number = 79 };
-    st.note[st.notes++] = (struct note) { .controller_number = 72 };
+    st.controller_number[st.offsets++] = 110;
+    st.controller_number[st.offsets++] = 111;
+    st.controller_number[st.offsets++] = 112;
+    st.controller_number[st.offsets++] = 113;
+    st.controller_number[st.offsets++] = 17;
+    st.controller_number[st.offsets++] = 91;
+    st.controller_number[st.offsets++] = 79;
+    st.controller_number[st.offsets++] = 72;
 
-    for(size_t i = 0; i < st.notes; i++) {
-        st.note[i].offset = 0;
-        info("note (%zu): controller number=%u", i+1,
-             st.note[i].controller_number);
+    for(size_t i = 0; i < st.offsets; i++) {
+        info("note (%zu): controller number=%u", i+1, st.controller_number[i]);
+    }
+
+    for(size_t i = 0; i < LENGTH(st.stored); i++) {
+        st.stored[i].i = i;
     }
 
     reset_active_notes(&st);
